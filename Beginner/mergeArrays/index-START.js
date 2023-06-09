6e7579bd1acb8bc34eeba71b51dc60a2d44a52d3
busma13 <peterluitjens@gmail.com>
2023-06-09 06:25:25 -0700

    Write solutions to problem using filter and Set.

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,28 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// function mergeArrays(...arrays) {
+
+//     let jointArray = []
+    
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+
+//     return [...new Set([...jointArray])]
+// }
+
+// function mergeArrays() {
+//     return [...new Set([...arguments].flat())]
+// }
+
 function mergeArrays(...arrays) {
-
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
-
-    return [...new Set([...jointArray])]
-
-    
+    let finalResult = []
+    for (const array of arrays) {
+        finalResult = [...finalResult, ...array]
+    }
+    finalResult = finalResult.filter((item, idx) => finalResult.indexOf(item) === idx)
+    return finalResult
 }
-
 
 module.exports = mergeArrays
